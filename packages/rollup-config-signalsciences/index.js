294bd5599054881587901c3e47eafa4fb87b406c
John <john@signalsciences.com>
2019-10-06 18:43:48 -0700

    chore(rollup-config-signalsciences): change build target logic

--- packages/rollup-config-signalsciences/index.js
+++ packages/rollup-config-signalsciences/index.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const babel = require('rollup-plugin-babel')
-const bundleSize = require('rollup-plugin-bundle-size')
-const commonjs = require('rollup-plugin-commonjs')
-const resolve = require('rollup-plugin-node-resolve')
-const { terser } = require('rollup-plugin-terser')
+const path = require("path");
+const babel = require("rollup-plugin-babel");
+const bundleSize = require("rollup-plugin-bundle-size");
+const commonjs = require("rollup-plugin-commonjs");
+const resolve = require("rollup-plugin-node-resolve");
+const { terser } = require("rollup-plugin-terser");
 
 /* eslint-disable-next-line import/no-dynamic-require */
-const pkg = require(path.resolve(process.cwd(), './package.json'))
+const pkg = require(path.resolve(process.cwd(), "./package.json"));
 
 const banner = `/*!
  *
- * Copyright (c) ${(new Date()).getFullYear()} ${pkg.author}
+ * Copyright (c) ${new Date().getFullYear()} ${pkg.author}
  *
  * This code is licensed under the ${pkg.license} license.
  *
@@ -19,60 +19,69 @@ const banner = `/*!
  * ${pkg.homepage}
  *
  */
-`
+`;
+
+const umd = pkg["umd:main"];
+const cjs = pkg.main;
+const esm = pkg.module;
+
+if (!cjs || !esm) {
+  console.error(
+    "\n!! Error: `main` and `module` build targets are required.\n"
+  );
+  process.exit(1);
+}
+
+if (!umd) {
+  console.warn("\n!! Warning: Build target missing for `umd:main`\n");
+}
 
 module.exports = [
-  {
-    input: 'src/index.js',
+  umd && {
+    input: "src/index.js",
     output: {
       banner,
       name: pkg.name,
-      file: pkg.browser,
-      format: 'umd',
+      file: umd,
+      format: "umd",
       globals: {
-        react: 'React',
-      },
+        react: "React"
+      }
     },
-    external: [
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
+    external: [...Object.keys(pkg.peerDependencies || {})],
     plugins: [
       resolve(),
       babel(),
       commonjs(),
-      process.env.NODE_ENV === 'production'
-        && terser({
+      process.env.NODE_ENV === "production" &&
+        terser({
           ecma: 5,
           output: {
-            comments: /^!/,
+            comments: /^!/
           },
-          warnings: true,
+          warnings: true
         }),
-      bundleSize(),
-    ],
+      bundleSize()
+    ]
   },
   {
-    input: 'src/index.js',
+    input: "src/index.js",
     output: [
       {
         banner,
-        file: pkg.main,
-        format: 'cjs',
+        file: cjs,
+        format: "cjs"
       },
-      {
+      module && {
         banner,
-        file: pkg.module,
-        format: 'es',
-      },
+        file: esm,
+        format: "es"
+      }
     ],
     external: [
       ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
     ],
-    plugins: [
-      resolve(),
-      babel(),
-      bundleSize(),
-    ],
-  },
-]
+    plugins: [resolve(), babel(), bundleSize()]
+  }
+].filter(Boolean);
