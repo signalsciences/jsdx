a155ade02b84e0a4fef5e72b06fafa8d1a105f6e
John <john@signalsciences.com>
2019-10-06 18:50:46 -0700

    chore(eslint-config-signalsciences): add prettier, simplify config

--- packages/eslint-config-signalsciences/index.js
+++ packages/eslint-config-signalsciences/index.js
@@ -1,36 +1,27 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: [
-    'eslint:recommended',
+    'airbnb',
     'plugin:compat/recommended',
     'plugin:flowtype/recommended',
-    'plugin:jest/recommended',
-    'airbnb',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/react'
   ],
   plugins: [
     'compat',
     'flowtype',
-    'jest',
-    'react',
+    'prettier'
+  ],
+  overrides: [
+    Object.assign(
+      {
+        files: ['**/*.test.js', '**/*.spec.js'],
+        env: { jest: true }
+      }
+    )
   ],
   rules: {
-    semi: ['error', 'never'],
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'never',
-    }],
-    'jsx-quotes': ['error', 'prefer-single'],
-    'space-before-function-paren': ['error', 'always'],
-    'react/jsx-curly-spacing': ['error', 'always'],
-    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
-    'react/require-default-props': 'off',
-    'react/state-in-constructor': ['error', 'never'],
-    'react/static-property-placement': ['error', 'static public field'],
-    'import/no-extraneous-dependencies': 'off',
-    // flow vs default props
-    'react/default-props-match-prop-types': 'off',
-  },
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }]
+  }
 }
